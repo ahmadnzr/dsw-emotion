afbd89f2972a971356fc9c70bbfd6170a51b8705
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-10-18 23:10:08 +0700

    fix(eslint): interface name error

--- packages/ui/organisms/ExampleOrganisms.tsx
+++ packages/ui/organisms/ExampleOrganisms.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
-interface Props {
+interface ExampleOrganismsProps {
   children?: React.ReactNode;
 }
 
-export const ExampleOrganisms = ({ children }: Props) => {
+export const ExampleOrganisms = ({ children }: ExampleOrganismsProps) => {
   return (
     <div>
       <h1>ExampleOrganisms</h1>
