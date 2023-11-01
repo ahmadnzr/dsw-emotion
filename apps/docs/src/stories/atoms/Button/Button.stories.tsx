a213829632a07bb77a14750fa849671304b0966b
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-11-01 10:23:14 +0700

    feat(docs-ui): Button stories

--- apps/docs/src/stories/atoms/Button/Button.stories.tsx
+++ apps/docs/src/stories/atoms/Button/Button.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
 import { Button } from "ui/components/atoms/Button";
 
 const meta = {
@@ -14,9 +13,26 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Contained: Story = {
   args: {
     children: "Submit",
     type: "button",
+    variant: "contained",
+  },
+};
+
+export const Outlined: Story = {
+  args: {
+    children: "Submit",
+    type: "button",
+    variant: "outlined",
+  },
+};
+
+export const Text: Story = {
+  args: {
+    children: "Submit",
+    type: "button",
+    variant: "text",
   },
 };
