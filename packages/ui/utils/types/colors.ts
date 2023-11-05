29a8538e51ebea5a0a097bfa5985317b850e82dd
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-11-05 21:29:46 +0700

    fix(ui): update theme config color

--- packages/ui/utils/types/colors.ts
+++ packages/ui/utils/types/colors.ts
@@ -1,10 +1,34 @@
-type ColorKeys = "primary" | "neutral";
+// type ColorKeys = "primary" | "neutral";
+//
+// type ColorValue = Record<
+//   "hard" | "medium" | "soft" | "ultrasoft" | "light",
+//   string
+// >;
 
-type ColorValue = Record<
-  "hard" | "medium" | "soft" | "ultrasoft" | "light",
-  string
->;
-
-type ColorType = Record<ColorKeys, ColorValue>;
+// type ColorType = Record<ColorKeys, ColorValue>;
+interface ColorType {
+  primary: {
+    dark: string;
+    hard: string;
+    medium: string;
+    soft: string;
+  };
+  neutral: {
+    hard: string;
+    medium: string;
+    soft: string;
+  };
+  error: {
+    hard: string;
+    medium: string;
+    soft: string;
+  };
+  success: {
+    hard: string;
+    medium: string;
+    soft: string;
+  };
+  white: string;
+}
 
 export { type ColorType };
