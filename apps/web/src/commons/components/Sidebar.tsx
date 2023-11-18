6998480395691c4c81472c10b87e6a4fd8e20255
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-11-19 00:29:26 +0700

    feat(web): sidebar

--- apps/web/src/commons/components/Sidebar.tsx
+++ apps/web/src/commons/components/Sidebar.tsx
@@ -2,26 +2,60 @@
 
 import styled from "@emotion/styled";
 import { useState } from "react";
+import { Icon } from "ui";
+
+const listMenu = [
+  {
+    id: 0,
+    title: "Dashboard",
+    childs: [],
+  },
+  {
+    id: 1,
+    title: "Tabungan",
+    childs: [
+      {
+        id: 0,
+        title: "Rekening Koran",
+      },
+      {
+        id: 1,
+        title: "Giro",
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
   const [active, setActive] = useState(false);
 
+  const onClickMenu = (isHaveChild: boolean) => {
+    if (!isHaveChild) {
+      return;
+    }
+
+    setActive((val) => !val);
+  };
+
   return (
     <SidebarStyled>
-      <GroupMenu>
-        <MenuTitle
-          onClick={() => {
-            setActive(!active);
-          }}
-        >
-          Dashboard
-        </MenuTitle>
-        <ListItem active={active}>
-          <MenuItem>Oke 1</MenuItem>
-          <MenuItem>OKe 2</MenuItem>
-          <MenuItem>OKE 3</MenuItem>
-        </ListItem>
-      </GroupMenu>
+      {listMenu.map((item) => (
+        <GroupMenu key={item.id}>
+          <MenuTitle
+            onClick={() => {
+              onClickMenu(Boolean(item.childs.length));
+            }}
+          >
+            <Icon color="#0061A7" name="building-library" size="md" />
+            {item.title}
+          </MenuTitle>
+          {item.childs.map((child) => (
+            <ListItem active={active} key={child.id}>
+              <MenuItem>{child.title}</MenuItem>
+            </ListItem>
+          ))}
+        </GroupMenu>
+      ))}
     </SidebarStyled>
   );
 };
@@ -38,13 +72,35 @@ const SidebarStyled = styled.div({
 const GroupMenu = styled.div({});
 
 const MenuTitle = styled.div({
+  padding: "0 20px",
+  height: "32px",
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
   cursor: "pointer",
+  transition: "0.3s ease",
+
+  "&:hover": {
+    backgroundColor: "#EAF2FF",
+  },
 });
 
 const ListItem = styled.div({}, ({ active }: { active: boolean }) => ({
   display: active ? "block" : "none",
 }));
 
-const MenuItem = styled.div({});
+const MenuItem = styled.div({
+  paddingLeft: "50px",
+  height: "32px",
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  cursor: "pointer",
+  transition: "0.3s ease",
+
+  "&:hover": {
+    backgroundColor: "#EAF2FF",
+  },
+});
 
 export default Sidebar;
