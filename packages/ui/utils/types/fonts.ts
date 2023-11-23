fd023ad8f16995a3a23b55ed43e2213c48b2795c
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-11-23 22:27:23 +0700

    fix(ui): update theme config

--- packages/ui/utils/types/fonts.ts
+++ packages/ui/utils/types/fonts.ts
@@ -1,12 +1,11 @@
-type FontSize = "xs" | "sm" | "md" | "lg";
+type FontSize = "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
 type FontWeight = "bold" | "semibold" | "normal" | "thin";
-type HeadingType = "h1" | "h2";
+type HeadingType = "h1" | "h2" | "h3" | "h4";
 
 interface FontType {
   family: string;
   size: Record<FontSize, string>;
   weight: Record<FontWeight, number>;
-  heading: Record<HeadingType, string>;
 }
 
 export { type FontType, type FontSize, type FontWeight, type HeadingType };
